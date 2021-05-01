adbd87d8131800d835819c9ca3a7f399cd5b9276
SudhakarKuma <sm221294@gmail.com>
2021-05-01 14:41:01 +0530

    Update LDR

--- user-code/ldr/arduino/ldr-led/ldr-led.ino
+++ user-code/ldr/arduino/ldr-led/ldr-led.ino
@@ -1,17 +1,17 @@
-int val1;
-int i=1;
+int val;
+int i = 1;
 void setup() {
-pinMode(11,OUTPUT);          // LED Pin
+pinMode(11, OUTPUT);          // LED Pin
 Serial.begin(115200);
-for(i=1;i<=2000;i++){
-   val1=analogRead(A5);      // Value of LDR
-   Serial.println(val1);
-   if(val1<300){             // Threshold
-      digitalWrite(11,HIGH);
+for(i = 1; i <= 2000; i++){
+   val = analogRead(A5);      // Value of LDR
+   // Serial.println(val);
+   if(val < 300){             // Threshold
+      digitalWrite(11, HIGH);
    }
    else
    {
-      digitalWrite(11,LOW);
+      digitalWrite(11, LOW);
    }
 }
 }
--- user-code/ldr/arduino/ldr-read/ldr-read.ino
+++ user-code/ldr/arduino/ldr-read/ldr-read.ino
@@ -1,10 +1,10 @@
-int val1;                 // for LDR
-int i=1;
+int val;                 // for LDR
+int i = 1;
 void setup() {
 Serial.begin(115200);
-for(i=1;i<=20;i++){
-   val1=analogRead(A5);   // value of LDR
-   Serial.println(val1);  // for display
+for(i = 1; i <= 20; i++){
+   val = analogRead(A5);   // value of LDR
+   Serial.println(val);  // for display
    delay(500);
   }
 }
